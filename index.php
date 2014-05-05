f28a66bf3685036233846d4b0fd28152cec5ba2f
Sandeep Shetty <sandeep.shetty@gmail.com>
2014-05-05 06:38:45 +0530

    Updated index.php to use recent changes (app\ENV, files autoload) in phpish/app.

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
 
 
-	define('APPLICATION_ENV', preg_match('/^127\.0\.0\.1.*/', $_SERVER['HTTP_HOST']) ? 'development' : 'production');
-	require __DIR__.'/conf/'.APPLICATION_ENV.'.conf.php';
-
-	require __DIR__.'/vendor/phpish/app/app.php';
-	require __DIR__.'/vendor/phpish/mysql/mysql.php';
-	require __DIR__.'/vendor/phpish/template/template.php';
-
+	require __DIR__.'/vendor/autoload.php';
 
 	use phpish\app;
 	use phpish\mysql;
 	use phpish\template;
+
+	require __DIR__.'/conf/'.app\ENV.'.conf.php';
 
 
 	app\get('.*', function (){
